d5f64e51aef63c6a396dd047b4b8a507cc221dab
Bogachevv <dog67688@gmail.com>
2024-10-19 18:55:52 +0300

    Multilang MMLU

---
+++ MMMLU/load_preds.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+langs="./langs.txt"
+run_name="GSOFT-cfg3"
+src_path="charisma:/home/vabogachev/OrthogonalFineTune/runs/Llama-3-8b-${run_name}/preds_0sh_0_"
+dst_path="./${run_name}/"
+
+
+while IFS= read -r lang
+do
+	echo "Loading ${lang}..."	
+	echo "${src_path}${lang}.bin"
+	echo "${dst_path}${lang}.bin"
+	scp "${src_path}${lang}.bin" "${dst_path}${lang}.bin"
+	echo ""
+done < $langs
